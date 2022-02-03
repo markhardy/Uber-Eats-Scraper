d2ed9ab2b8962a56fa7b4a005e990adba7e13310
Mark Hardy <markhardy.dev@gmail.com>
2022-02-03 15:27:39 -0500

    Working through popups
    Adding functionality to get through UberEats popups

--- uber-eats-scraper.py
+++ uber-eats-scraper.py
@@ -1,9 +1,16 @@
 from bs4 import BeautifulSoup
 from urllib.request import Request, urlopen
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 import json
 import ssl
 import urllib
 import time
+
+options = webdriver.ChromeOptions()
+options.add_argument('--headless')
+driver = webdriver.Chrome(options=options)
 
 def run():
     headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)'} # set the headers
@@ -16,18 +23,37 @@ def run():
     state = 'dc'
     url = "https://www.ubereats.com/city/" + city.lower() + "-" + state.lower()
 
+    driver.get(url)
+    time.sleep(5)
+    
+    label = driver.find_element(By.ID, 'location-typeahead-home-input')
+    label.send_keys('washington d.c.')
+    label.send_keys(u'\ue007')
+    
+    time.sleep(5)
+
+    url = 'https://www.ubereats.com/feed?diningMode=DINE_IN&pl=JTdCJTIyYWRkcmVzcyUyMiUzQSUyMldhc2hpbmd0b24lMjIlMkMlMjJyZWZlcmVuY2UlMjIlM0ElMjJDaElKVy1UMld0N0d0NGtSS2wySTFDSkZVc0klMjIlMkMlMjJyZWZlcmVuY2VUeXBlJTIyJTNBJTIyZ29vZ2xlX3BsYWNlcyUyMiUyQyUyMmxhdGl0dWRlJTIyJTNBMzguOTA2NzklMkMlMjJsb25naXR1ZGUlMjIlM0EtNzcuMDM3OTY2NyU3RA%3D%3D'
+    driver.get(url)
+
+    buttons = driver.find_elements(By.CLASS_NAME, 'gy')
+    for button in buttons:
+        print(button.text)
+
+
     req = Request(url, headers={'User-Agent' : 'Mozilla/5.0'})
     page = urlopen(req).read()
+    print(page)
     soup = BeautifulSoup(page, 'html.parser')
-
+    print('page downloaded')
+    
     for x in soup.findAll('h3'): # find the name of the restaurant
         x_url = x.find_parent(name='a', href=True)
         print('name of restaurant: ' + x.text)
         a = x.findNext('div', attrs = {'class' : 'ag bk'})
-        click_restaurant(x_url['href'])
+        click_restaurant_js(x_url['href'])
         if a is not None:
-            time = a.findNext('span').text
-            time = time.replace("–"," to ")
+            hours = a.findNext('span').text
+            hours = hours.replace("–"," to ")
             pre_rating = x.findNext('div', attrs = {'class' : 'spacer _16'})
             rating = pre_rating.findNext('div')
             key = x.text
@@ -44,12 +70,74 @@ def write():
     with open('final_result.json', 'w+', encoding='utf-8') as out: # writing the ginal file
         json.dump(data, out, indent=4, ensure_ascii=False)
 
+def click_restaurant_js(url):
+
+    restaurant_url = 'https://ubereats.com' + url
+    driver.get(restaurant_url)
+    time.sleep(8)
+
+    #pictures = driver.find_elements(By.CSS_SELECTOR, 'picture source')
+    pictures = driver.find_elements(By.TAG_NAME, 'picture')
+    for picture in pictures:
+        source = picture.find_element(By.TAG_NAME, 'source')
+        srcset = source.get_attribute('srcset')
+        print(srcset)
+        img = picture.find_element(By.TAG_NAME, 'img')
+        alt = img.get_attribute('alt')
+        print(alt)
+
+
+    '''
+    pictures = driver.find_elements(By.TAG_NAME, 'ul')
+
+    #pictures = driver.find_elements(By.TAG_NAME, 'picture')
+    #pictures = driver.find_elements(By.CSS_SELECTOR, 'picture')
+    print(pictures)
+    for picture in pictures:
+        print(picture)
+        picture_child = picture.find_element(By.TAG_NAME, 'li')
+        print(picture_child)
+        #picture_child = picture.find_element(By.TAG_NAME, 'img')
+        #print(picture_child.get_attribute('alt'))
+        #print(picture_child.get_attribute('src'))
+    '''
+    
+
 def click_restaurant(url):
     restaurant_url = 'https://ubereats.com' + url
     print('restaurant url: ' + restaurant_url)
     req = Request(restaurant_url, headers={'User-Agent' : 'Mozilla/5.0'})
     page = urlopen(req).read()
+    time.sleep(5)
     soup = BeautifulSoup(page, 'html.parser')
+    print(soup.picture)
+    #print(soup.prettify())
+    pictures = soup.find_all('picture')
+    print(pictures)
+    for picture in pictures:
+        print(picture)
+        children = picture.findChildren(recursive=False)
+        picture_data = []
+        for child in children:
+            child_as_string = str(child)
+            child_split = child_as_string.split('"')
+            if len(child_split) > 3 and child_split[0] == '<source type=':
+                src = child_split[3]
+                picture_data.append(src)
+            elif len(child_split) > 3 and child_split[0] == '<img alt=':
+                alt = child_split[1]
+                picture_data.append(alt)
+            else:
+                print('srcset has fewer than 3 elements')
+        for data in picture_data:
+            print(data)
+
+    
+
+        
+    
+
+    '''
     images = soup.find_all('img')
     images_as_strings = []
     for image in images:
@@ -66,5 +154,9 @@ def click_restaurant(url):
                 src = img[7]
                 print('src: ' + src)
                 print('alt: ' + alt)
+            else:
+                print('no aria')
+                print(img)
+    '''
 
 run()
